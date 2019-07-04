abf5f3b8657fbad95685595a25746f7b3985f5ab
smndhm <simon.duhem@gmail.com>
2019-07-05 00:48:40 +0200

    IILaR: Set interval screen

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# coding: utf8
 
 # Imports
 import sys
@@ -10,9 +11,14 @@ import pygame
 import pygame.camera
 from pygame.locals import *
 
+timeInterval = 5
+timeIntervalStep = 5
+timeIntervalMin = 5
+
 ## ----------------------------------------------------------------------------------------------------
 ## Class Screen
 ## ----------------------------------------------------------------------------------------------------
+
 class Screen():
 	#__init__
 	def __init__(self):
@@ -20,6 +26,8 @@ class Screen():
 			'white':(255,255,255), 
 			'black':(0,0,0)
 		}	
+		self.fontColor = self.colors['white']
+		self.backgroundColor = self.colors['black']
 		self.default_screen_resolution = (1000, 1000)
 		self.screen = pygame.display.set_mode(self.default_screen_resolution)
 		self.home()
@@ -31,18 +39,28 @@ class Screen():
 		self.background.fill(color)
 		self.screen.blit(self.background, (0, 0))
 
+	## --------------------------------------------------------------------------------------------------
+
 	def home(self):
-		# Background
-		self.setBackgroundColor(self.colors['black'])
-		# Text
-		text = font.render("PRESS TO START", True, self.colors['white'])
+		
+		# Background --------------------------------------
+		
+		self.setBackgroundColor(self.backgroundColor)
+		
+		# Text --------------------------------------------
+		
+		text = font.render("PRESS TO START", True, self.fontColor)
 		textrect = text.get_rect()
 		textrect.centerx = self.screen.get_rect().centerx
 		textrect.centery = self.screen.get_rect().centery
 		self.screen.blit(text, textrect)
-		# update screen
+		
+		# update screen -----------------------------------
+		
 		pygame.display.flip()
-		# Buttons
+		
+		# Buttons -----------------------------------------
+		
 		btnKey1.when_pressed = self.pictureInterval
 		btnKey2.when_pressed = self.pictureInterval
 		btnKey3.when_pressed = self.pictureInterval
@@ -52,14 +70,93 @@ class Screen():
 		btnJoystickRight.when_pressed = self.pictureInterval
 		btnJoystickPress.when_pressed = self.pictureInterval
 
+	## --------------------------------------------------------------------------------------------------
+
 	def pictureInterval(self):
-		# Background
-		self.setBackgroundColor(self.colors['white'])
-		# Text
 		
-		# update screen
+		# Functions ---------------------------------------
+		
+		def displayTimeInterval(time):
+			surfaceTime = pygame.Surface((400, 300))
+			surfaceTime.fill(self.backgroundColor)
+			textTime = font.render(str(time), True, self.fontColor)
+			textTimeRect = textTime.get_rect()
+			textTimeRect.centerx = 200
+			textTimeRect.centery = 150
+			surfaceTime.blit(textTime, textTimeRect)
+			self.screen.blit(surfaceTime, (300, 350))
+
+		def pictureIntervalTimeAdd():
+			global timeInterval, timeIntervalStep
+			timeInterval+=timeIntervalStep
+			displayTimeInterval(timeInterval)
+			pygame.display.flip()
+
+		def pictureIntervalTimeLess():
+			global timeInterval, timeIntervalStep, timeIntervalMin
+			if timeInterval-timeIntervalStep >= timeIntervalMin:
+				timeInterval-=timeIntervalStep
+				displayTimeInterval(timeInterval)
+				pygame.display.flip()
+
+		# Background --------------------------------------
+		
+		self.setBackgroundColor(self.backgroundColor)
+		
+		# Text --------------------------------------------
+		
+		textTitle = font.render("PICTURE INTERVAL:", True, self.fontColor)
+		textTitleRect = textTitle.get_rect()
+		textTitleRect.centerx = 500
+		textTitleRect.centery = 175
+		self.screen.blit(textTitle, textTitleRect)
+
+		textTimeLess = font.render(u"◀", True, self.fontColor)
+		textTimeLessRect = textTimeLess.get_rect()
+		textTimeLessRect.centerx = 150
+		textTimeLessRect.centery = 500
+		self.screen.blit(textTimeLess, textTimeLessRect)
+
+		textTimeMore = font.render(u"▶", True, self.fontColor)
+		textTimeMoreRect = textTimeMore.get_rect()
+		textTimeMoreRect.centerx = 850
+		textTimeMoreRect.centery = 500
+		self.screen.blit(textTimeMore, textTimeMoreRect)
+
+		textTimeUnit = font.render("SECONDS", True, self.fontColor)
+		textTimeUnitRect = textTimeUnit.get_rect()
+		textTimeUnitRect.centerx = 500
+		textTimeUnitRect.centery = 825
+		self.screen.blit(textTimeUnit, textTimeUnitRect)
+
+		displayTimeInterval(timeInterval)
+
+		# update screen -----------------------------------
+		
 		pygame.display.flip()
-		# Buttons
+		
+		# Buttons -----------------------------------------
+
+		btnKey1.when_pressed = self.preview
+		btnKey2.when_pressed = self.preview
+		btnKey3.when_pressed = self.preview
+		btnJoystickUp.when_pressed = None
+		btnJoystickDown.when_pressed = None
+		btnJoystickleft.when_pressed = pictureIntervalTimeLess
+		btnJoystickRight.when_pressed = pictureIntervalTimeAdd
+		btnJoystickPress.when_pressed = self.preview
+
+	## --------------------------------------------------------------------------------------------------
+
+	def preview(self):
+		# Functions ---------------------------------------
+		# Background --------------------------------------
+		self.setBackgroundColor(self.colors['white'])
+		# Text --------------------------------------------
+		# update screen -----------------------------------
+		pygame.display.flip()
+		# Buttons -----------------------------------------
+
 
 ## ----------------------------------------------------------------------------------------------------
 ## ----------------------------------------------------------------------------------------------------
